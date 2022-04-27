8b6015ff6474af9b68d7e8a24281a8405091a785
RobinHoo <robinhoo990512@gmail.com>
2022-04-28 00:39:39 +0800

    MVVM architecture done

--- app/src/main/java/com/example/simplecalculator/model/CalculatorData.kt
+++ app/src/main/java/com/example/simplecalculator/model/CalculatorData.kt
@@ -1,9 +1,8 @@
 package com.example.simplecalculator.model
 
 data class CalculatorData(
-    var newState: Boolean = true,
-    var hasPoint: Boolean = false,
-    var oldNum: String = "",
-    var finalNum: String = "",
-    var calculateType: String = ""
+    var text: String? = "",
+    var newText: String? = "",
+    var expression: String? = "",
+    var result: String? = ""
 )
--- app/src/main/java/com/example/simplecalculator/viewModel/CalculatorActivityViewModel.kt
+++ app/src/main/java/com/example/simplecalculator/viewModel/CalculatorActivityViewModel.kt
@@ -4,73 +4,14 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.expressionparserutil.ExpressionParserUtil
+import com.example.simplecalculator.model.CalculatorData
 
 class CalculatorActivityViewModel : ViewModel() {
 
     private val _displayTextLiveData = MutableLiveData<String>()
+    private val model = CalculatorData()
     val displayTextLiveData: LiveData<String>
         get() = _displayTextLiveData
-
-    private val calculator = ExpressionParserUtil()
-
-    fun numberInput(buttonValue: Int) {
-        val text = _displayTextLiveData.value
-        val newText = text + buttonValue.toString()
-        _displayTextLiveData.value = newText
-    }
-
-    fun symbolInput(symbol: String) {
-        val text = _displayTextLiveData.value!!
-
-        fun updateDisplay() {
-            val newText = "${text}$symbol"
-            _displayTextLiveData.value = newText
-        }
-
-        when (symbol) {
-            "÷", "x" -> if (text.isNotEmpty()) {
-                if (text.last().isDigit() || text.last() == ')') updateDisplay()
-            }
-
-            "+", "-" -> updateDisplay()
-
-            "(" -> if (text.isNotEmpty()) {
-                if (text.last() == 'x' ||
-                    text.last() == '÷' ||
-                    text.last() == '-' ||
-                    text.last() == '+' ||
-                    text.last() == '('
-                ) updateDisplay() else if (text.isNotEmpty() || text.last() == ')') {
-                    val newText = "${text}x$symbol"
-                    _displayTextLiveData.value = newText
-                }
-            }
-
-            ")" -> if (text.isNotEmpty()) {
-                if (text.last().isDigit() ||
-                    text.last() == ')' ||
-                    text.last() == '('
-                ) updateDisplay()
-            }
-
-            "." -> if (text.isNotEmpty()) {
-                if (text.last().isDigit()) updateDisplay()
-            }
-        }
-    }
-
-    fun equals() {
-
-        if (calculator.parenthesesCheck(_displayTextLiveData.value.toString())) {
-            val expression = _displayTextLiveData.value!!
-            val result = calculator.calc(expression)
-            _displayTextLiveData.value = result
-        }
-    }
-
-    fun clear() {
-        _displayTextLiveData.value = ""
-    }
 
     init {
         _displayTextLiveData.value = ""
@@ -83,4 +24,65 @@ class CalculatorActivityViewModel : ViewModel() {
     val opParenthesesSymbol = "("
     val clParenthesesSymbol = ")"
     val commaSymbol = "."
+
+    private val calculator = ExpressionParserUtil()
+
+    fun numberInput(buttonValue: Int) {
+        model.text = _displayTextLiveData.value
+        model.newText = model.text + buttonValue.toString()
+        _displayTextLiveData.value = model.newText!!
+    }
+
+    fun symbolInput(symbol: String) {
+        model.text = _displayTextLiveData.value!!
+
+        fun updateDisplay() {
+            model.newText = "${model.text}$symbol"
+            _displayTextLiveData.value = model.newText!!
+        }
+
+        when (symbol) {
+            "÷", "x" -> if (model.text!!.isNotEmpty()) {
+                if (model.text!!.last().isDigit() || model.text!!.last() == ')') updateDisplay()
+            }
+
+            "+", "-" -> updateDisplay()
+
+            "(" -> if (model.text!!.isNotEmpty()) {
+                if (model.text!!.last() == 'x' ||
+                    model.text!!.last() == '÷' ||
+                    model.text!!.last() == '-' ||
+                    model.text!!.last() == '+' ||
+                    model.text!!.last() == '('
+                ) updateDisplay() else if (model.text!!.isNotEmpty() || model.text!!.last() == ')') {
+                    val newText = "${model.text}x$symbol"
+                    _displayTextLiveData.value = newText
+                }
+            }
+
+            ")" -> if (model.text!!.isNotEmpty()) {
+                if (model.text!!.last().isDigit() ||
+                    model.text!!.last() == ')' ||
+                    model.text!!.last() == '('
+                ) updateDisplay()
+            }
+
+            "." -> if (model.text!!.isNotEmpty()) {
+                if (model.text!!.last().isDigit()) updateDisplay()
+            }
+        }
+    }
+
+    fun equals() {
+        if (calculator.parenthesesCheck(_displayTextLiveData.value.toString())) {
+            model.expression = _displayTextLiveData.value!!
+            model.result = calculator.calc(model.expression!!)
+            _displayTextLiveData.value = model.result!!
+        }
+    }
+
+    fun clear() {
+        _displayTextLiveData.value = ""
+    }
+
 }
