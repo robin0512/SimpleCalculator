fcb876c7b429f370aea69adf16f3d6e09d61e336
RobinHoo <robinhoo990512@gmail.com>
2022-04-27 23:37:43 +0800

    simple code

--- app/src/main/java/com/example/simplecalculator/CalculatorActivity.kt
+++ app/src/main/java/com/example/simplecalculator/CalculatorActivity.kt
@@ -3,7 +3,6 @@ package com.example.simplecalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
-import android.text.method.ScrollingMovementMethod
 import android.view.View
 import com.example.simplecalculator.databinding.CalculatorBinding
 import com.example.simplecalculator.viewModel.CalculatorActivityViewModel
@@ -15,14 +14,11 @@ class CalculatorActivity : AppCompatActivity() {
         val binding: CalculatorBinding = CalculatorBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        binding.display.movementMethod = ScrollingMovementMethod()
-
         val viewModel = ViewModelProvider(this).get(CalculatorActivityViewModel::class.java)
 
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
 
-        // Hide the status bar.
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
         supportActionBar?.hide()
     }
--- app/src/main/java/com/example/simplecalculator/viewModel/CalculatorActivityViewModel.kt
+++ app/src/main/java/com/example/simplecalculator/viewModel/CalculatorActivityViewModel.kt
@@ -5,38 +5,21 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.expressionparserutil.ExpressionParserUtil
 
-class CalculatorActivityViewModel : ViewModel()  {
-    //LiveData object used to update the UI expressions display
+class CalculatorActivityViewModel : ViewModel() {
+
     private val _displayTextLiveData = MutableLiveData<String>()
     val displayTextLiveData: LiveData<String>
         get() = _displayTextLiveData
 
-    //LiveData object used to update the UI error messages display
-    private val _errorTextLiveData = MutableLiveData<String>()
-    val errorTextLiveData: LiveData<String>
-        get() = _errorTextLiveData
-
-    //Expression parser and calculator imported via jitpack.
     private val calculator = ExpressionParserUtil()
 
-    //Error messages to be displayed in case of invalid input
-    private val genericError = "invalid expression"
-    private val parenthesesError = "mismatched parentheses"
-    private val emptyExpressionError = "enter an expression"
-
-    //Click function for numeric input
     fun numberInput(buttonValue: Int) {
-        if (_errorTextLiveData.value.toString().isNotEmpty()) clearErrorMessage()
-
         val text = _displayTextLiveData.value
         val newText = text + buttonValue.toString()
         _displayTextLiveData.value = newText
     }
 
-    //Click function for symbols input - it stops the user from inputting some invalid expressions.
     fun symbolInput(symbol: String) {
-        if (_errorTextLiveData.value.toString().isNotEmpty()) clearErrorMessage()
-
         val text = _displayTextLiveData.value!!
 
         fun updateDisplay() {
@@ -52,11 +35,12 @@ class CalculatorActivityViewModel : ViewModel()  {
             "+", "-" -> updateDisplay()
 
             "(" -> if (text.isNotEmpty()) {
-                if (text.last() == 'x'||
-                    text.last() == '÷'||
-                    text.last() == '-'||
-                    text.last() == '+'||
-                    text.last() == '(') updateDisplay() else if (text.isNotEmpty() || text.last() == ')') {
+                if (text.last() == 'x' ||
+                    text.last() == '÷' ||
+                    text.last() == '-' ||
+                    text.last() == '+' ||
+                    text.last() == '('
+                ) updateDisplay() else if (text.isNotEmpty() || text.last() == ')') {
                     val newText = "${text}x$symbol"
                     _displayTextLiveData.value = newText
                 }
@@ -65,7 +49,8 @@ class CalculatorActivityViewModel : ViewModel()  {
             ")" -> if (text.isNotEmpty()) {
                 if (text.last().isDigit() ||
                     text.last() == ')' ||
-                    text.last() == '(') updateDisplay()
+                    text.last() == '('
+                ) updateDisplay()
             }
 
             "." -> if (text.isNotEmpty()) {
@@ -74,64 +59,23 @@ class CalculatorActivityViewModel : ViewModel()  {
         }
     }
 
-    //Delete function - deletes last character from display
-    fun delete() {
-        if (_errorTextLiveData.value.toString().isNotEmpty()) clearErrorMessage()
-
-        val text = _displayTextLiveData.value!!
-        val newText = text.dropLast(1)
-        _displayTextLiveData.value = newText
-    }
-
-    //Equal button - calls the "calc" function on the calculator object and handles
-    //invalid expressions, displaying the error on the _errorTextLiveData.
     fun equals() {
-        if (_displayTextLiveData.value!!.isEmpty()) {
-            emptyExpressionError()
-            return
-        }
+
         if (calculator.parenthesesCheck(_displayTextLiveData.value.toString())) {
-            try {
-                val expression = _displayTextLiveData.value!!
-                val result = calculator.calc(expression)
-                _displayTextLiveData.value = result
-            } catch (e: Exception) {
-                genericError()
-            }
-        } else parenthesesError()
+            val expression = _displayTextLiveData.value!!
+            val result = calculator.calc(expression)
+            _displayTextLiveData.value = result
+        }
     }
 
-    //Generic error in case of unexpected invalid expressions that might throw exceptions.
-    private fun genericError() {
-        _errorTextLiveData.value = genericError
-    }
-
-    //In case of invalid parentheses configuration
-    private fun parenthesesError() {
-        _errorTextLiveData.value = parenthesesError
-    }
-
-    private fun emptyExpressionError() {
-        _errorTextLiveData.value = emptyExpressionError
-    }
-
-    //Clear button
     fun clear() {
-        if (_errorTextLiveData.value.toString().isNotEmpty()) clearErrorMessage()
-
         _displayTextLiveData.value = ""
-    }
-
-    //Clears the error messages
-    private fun clearErrorMessage() {
-        _errorTextLiveData.value = ""
     }
 
     init {
         _displayTextLiveData.value = ""
     }
 
-    //Variables with symbols for xml access
     val divisionSymbol = "÷"
     val multiplicationSymbol = "x"
     val plusSymbol = "+"
